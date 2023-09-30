72346aab79417cd691f4f57e41eb8254daab92dc
MarkZagors <69710010+MarkZagors@users.noreply.github.com>
2023-10-01 00:27:47 +0300

    Added sequence tab

--- scripts/classes/Sequence.cs
+++ scripts/classes/Sequence.cs
@@ -1,7 +1,9 @@
+using System.Collections.Generic;
 using Godot;
 
 public class Sequence
 {
     public double Time { get; set; }
     public Control Node { get; set; }
+    public List<IComponent> Components { get; set; }
 }
--- scripts/editor/SequenceController.cs
+++ scripts/editor/SequenceController.cs
@@ -5,16 +5,56 @@ namespace Editor
 {
     public partial class SequenceController : Node
     {
+        [Export] public Tree SequenceTree { get; private set; }
         [Export] public Control TemplatesTab { get; private set; }
         [Export] public Control SequenceTab { get; private set; }
+        [Export] public Control CreationContainer { get; private set; }
 
         private Sequence _openedSequence;
 
+        public override void _Ready()
+        {
+            SequenceTab.GetNode<Label>("NoSelectionLabel").Visible = true;
+            SequenceTab.GetNode<Control>("TopSequenceBar").Visible = false;
+            SequenceTree.Visible = false;
+        }
+
         public void OpenSequence(Sequence sequence)
         {
+            bool newOpening = false;
+            if (_openedSequence != sequence) newOpening = true;
+
             _openedSequence = sequence;
 
+            if (newOpening) PopulateSequenceTree();
+
             OpenSequenceTab();
+        }
+
+        private void PopulateSequenceTree()
+        {
+            SequenceTab.GetNode<Label>("NoSelectionLabel").Visible = false;
+            SequenceTab.GetNode<Control>("TopSequenceBar").Visible = true;
+            SequenceTree.Visible = true;
+
+            SequenceTree.Clear();
+
+            TreeItem root = SequenceTree.CreateItem();
+            root.SetText(0, "Sequence");
+        }
+
+        private void CreateBullet()
+        {
+            TreeItem root = SequenceTree.GetRoot();
+            TreeItem bulletComponentTreeItem = root.CreateChild();
+            bulletComponentTreeItem.SetText(0, "Bullet");
+
+            CloseNewComponent();
+        }
+
+        private void CloseNewComponent()
+        {
+            CreationContainer.Visible = false;
         }
 
         public void OpenSequenceTab()
@@ -27,6 +67,31 @@ namespace Editor
         {
             TemplatesTab.Visible = true;
             SequenceTab.Visible = false;
+        }
+
+        public void OnClickNewComponent()
+        {
+            CreationContainer.Visible = true;
+        }
+
+        public void OnClickCloseNewComponent()
+        {
+            CloseNewComponent();
+        }
+
+        public void OnClickCreateBullet()
+        {
+            CreateBullet();
+        }
+
+        public void OnClickCreateBundle()
+        {
+
+        }
+
+        public void OnClickCreateSpawner()
+        {
+
         }
     }
 }
