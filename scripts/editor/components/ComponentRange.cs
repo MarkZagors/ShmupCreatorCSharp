b33d9d67c32b7958f243559aa370f820e3c34a97
MarkZagors <69710010+MarkZagors@users.noreply.github.com>
2023-10-04 20:54:23 +0300

    Lines added for ranges

--- scripts/editor/components/ComponentRange.cs
+++ scripts/editor/components/ComponentRange.cs
@@ -8,16 +8,27 @@ namespace Editor
     public partial class ComponentRange : Control, IComponent
     {
         [Export] public Control RangeControllerNode;
+        [Export] public Line2D RangeLineNode;
         private bool _rangeExpanded = true;
         private Control _currentHoldingPoint;
         private bool _rangeMouseDragging = false;
         private List<Control> _hoveringRangePoints = new();
+        private List<Control> _rangePointNodes = new();
 
         public override void _Ready()
         {
-            var point = GetNode<Control>("RangeContainer/RangeController/Points/Point1");
-            point.MouseEntered += () => RangePointMouseEntered(point);
-            point.MouseExited += () => RangePointMouseExited(point);
+            var point1 = GetNode<Control>("RangeContainer/RangeController/Points/Point1");
+            point1.MouseEntered += () => RangePointMouseEntered(point1);
+            point1.MouseExited += () => RangePointMouseExited(point1);
+
+            var point2 = GetNode<Control>("RangeContainer/RangeController/Points/Point2");
+            point2.MouseEntered += () => RangePointMouseEntered(point2);
+            point2.MouseExited += () => RangePointMouseExited(point2);
+
+            _rangePointNodes.Add(point1);
+            _rangePointNodes.Add(point2);
+
+            UpdateLines();
         }
 
         public override void _Process(double delta)
@@ -50,6 +61,20 @@ namespace Editor
                 _currentHoldingPoint.AnchorLeft = mousePos.X;
                 _currentHoldingPoint.AnchorBottom = mousePos.Y;
                 _currentHoldingPoint.AnchorTop = mousePos.Y;
+
+                UpdateLines();
+            }
+        }
+
+        private void UpdateLines()
+        {
+            RangeLineNode.ClearPoints();
+
+            List<Vector2> expandedPointList = GetExpandedPointList();
+            foreach (Vector2 point in expandedPointList)
+            {
+                Vector2 worldSpacePosition = RangeSpaceToWorldSpace(point);
+                RangeLineNode.AddPoint(worldSpacePosition);
             }
         }
 
@@ -75,6 +100,45 @@ namespace Editor
                 X = (float)Mathf.Clamp(mousePos.X / RangeControllerNode.Size.X, 0.0, 1.0),
                 Y = (float)Mathf.Clamp(mousePos.Y / RangeControllerNode.Size.Y, 0.0, 1.0),
             };
+        }
+
+        private Vector2 RangeSpaceToWorldSpace(Vector2 pos)
+        {
+            return new Vector2
+            {
+                X = pos.X * RangeControllerNode.Size.X,
+                Y = pos.Y * RangeControllerNode.Size.Y,
+            };
+        }
+
+        private List<Vector2> GetExpandedPointList()
+        {
+            List<Vector2> pointList = new();
+            foreach (Control rangePointNode in _rangePointNodes)
+            {
+                pointList.Add(new Vector2
+                {
+                    X = rangePointNode.AnchorRight,
+                    Y = rangePointNode.AnchorTop,
+                });
+            }
+            pointList.Sort((vecA, vecB) => vecA.X.CompareTo(vecB.X));
+
+            Vector2 firstPointNonExpanded = pointList[0];
+            pointList.Insert(0, new Vector2
+            {
+                X = 0.0f,
+                Y = firstPointNonExpanded.Y,
+            });
+
+            Vector2 lastPointNonExpanded = pointList[pointList.Count - 1];
+            pointList.Add(new Vector2
+            {
+                X = 1.0f,
+                Y = lastPointNonExpanded.Y,
+            });
+
+            return pointList;
         }
 
         public void RangePointMouseEntered(Control node)
