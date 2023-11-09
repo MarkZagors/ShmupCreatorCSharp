f4c33b5a38300b72344a669fd018a58b9bb55500
MarkZagors <69710010+MarkZagors@users.noreply.github.com>
2023-11-09 19:41:58 +0200

    Removed testing

--- test/EditorTest.cs
+++
@@ -1,9 +0,0 @@
-using System.Reflection;
-using Godot;
-using Chickensoft.GoDotTest;
-
-public partial class EditorTest : Node
-{
-    public override async void _Ready()
-    => await GoTest.RunTests(Assembly.GetExecutingAssembly(), this);
-}
