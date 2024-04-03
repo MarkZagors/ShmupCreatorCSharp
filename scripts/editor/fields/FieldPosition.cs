36d42b6c96489f3f9367301be9f24cc460ac3110
MarkZagors <69710010+MarkZagors@users.noreply.github.com>
2024-04-03 14:57:56 +0300

    Added position tracking to the field

--- scripts/editor/fields/FieldPosition.cs
+++ scripts/editor/fields/FieldPosition.cs
@@ -8,15 +8,55 @@ namespace Editor
         [Signal] public delegate void UpdateEventHandler();
         [Export] public Button PositionButton { get; private set; }
         public ModifierPosition ModifierPosition { get; private set; }
+        public bool IsSelecting { get; set; }
+        private SubViewportContainer _playerViewportContainer;
+        private Control _midContainer;
+        private readonly Vector2 VIEWPORT_SIZE = new Vector2(768, 1024); //CHANGE THIS WHEN CHANGING VIEWPORT SIZE
 
         public void Init(ModifierPosition modifierPosition)
         {
             ModifierPosition = modifierPosition;
         }
 
+        public override void _Ready()
+        {
+            _playerViewportContainer = (SubViewportContainer)GetTree().GetNodesInGroup("play_viewport")[0];
+            _midContainer = _playerViewportContainer.GetNode<Control>("../..");
+            UpdateUI();
+        }
+
+        public override void _Process(double delta)
+        {
+            if (!IsSelecting) return;
+
+            if (Input.IsActionJustPressed("mouse_click"))
+            {
+                var mousePos = GetViewport().GetMousePosition();
+                var viewportRect = _playerViewportContainer.GetRect();
+                viewportRect.Position = new Vector2(
+                    x: viewportRect.Position.X + _midContainer.Position.X,
+                    y: viewportRect.Position.Y + _midContainer.Position.Y
+                );
+                if (viewportRect.HasPoint(mousePos))
+                {
+                    Vector2 ratio = (mousePos - viewportRect.Position) / viewportRect.Size;
+                    Vector2 position = ratio * VIEWPORT_SIZE;
+                    ModifierPosition.Position = position;
+                    EmitSignal(SignalName.Update);
+                    UpdateUI();
+                }
+                IsSelecting = false;
+            }
+        }
+
         public void OnButtonPressed()
         {
-            GD.Print("Press button");
+            IsSelecting = true;
+        }
+
+        private void UpdateUI()
+        {
+            PositionButton.Text = $"[{ModifierPosition.Position.X:F0}, {ModifierPosition.Position.Y:F0}]";
         }
     }
 }
--- scripts/editor/modifiers/ModifierPosition.cs
+++ scripts/editor/modifiers/ModifierPosition.cs
@@ -1,8 +1,11 @@
+using Godot;
+
 namespace Editor
 {
     public class ModifierPosition : IModifier
     {
         required public ModifierID ID { get; set; }
         public bool Active { get; set; } = false;
+        public Vector2 Position { get; set; } = new(400, 200);
     }
 }
