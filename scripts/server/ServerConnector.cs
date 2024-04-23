b17c4563a5e9ea98828d50bd77d6fb4b6f80acfc
MarkZagors <69710010+MarkZagors@users.noreply.github.com>
2024-04-23 23:25:14 +0300

    refactor server status names

--- scripts/server/ServerConnector.cs
+++ scripts/server/ServerConnector.cs
@@ -10,47 +10,50 @@ public partial class ServerConnector : Node
     [Export] public LevelPickController LevelPickController { get; private set; }
     private HttpRequest _httpRequestNode;
     private string _requestLevelID = null;
-    private HTTPChain _requestChainIndex = 0;
-    private string _connectionIP = "234.0.0.1";
+    private HttpStatus _requestChainIndex = 0;
+    private string _connectionIP = "127.0.0.1";
 
-    private enum HTTPChain
+    private enum HttpStatus
     {
         IDLE,
-        INDEX,
-        PHASES,
-        ERROR,
+        POST_INDEX,
+        POST_PHASES,
+        POST_ERROR,
+        GET_LEVELS,
     }
 
     public override void _Ready()
     {
         _httpRequestNode = GetNode<HttpRequest>("HTTPRequest");
         _httpRequestNode.RequestCompleted += OnResponse;
+
+        GetLevels();
     }
 
     public void OnPublishButtonClick()
     {
         if (LevelPickController.SelectedLevelID == null) return;
-        if (_requestChainIndex != HTTPChain.IDLE)
+        if (_requestChainIndex != HttpStatus.IDLE)
         {
             GD.Print("http processing");
             return;
         }
-        _requestChainIndex = HTTPChain.INDEX;
+        _requestChainIndex = HttpStatus.POST_INDEX;
         _requestLevelID = LevelPickController.SelectedLevelID;
         PublishStep();
     }
 
     private void PublishStep()
     {
-        if (_requestChainIndex == HTTPChain.INDEX)
+        if (_requestChainIndex == HttpStatus.POST_INDEX)
             SendIndexJson(_requestLevelID);
-        else if (_requestChainIndex == HTTPChain.PHASES)
+        else if (_requestChainIndex == HttpStatus.POST_PHASES)
             SendPhasesJson(_requestLevelID);
-        else if (_requestChainIndex == HTTPChain.IDLE)
+        else if (_requestChainIndex == HttpStatus.IDLE)
             GD.Print("Request ended");
-        else if (_requestChainIndex == HTTPChain.ERROR)
+        else if (_requestChainIndex == HttpStatus.POST_ERROR)
         {
-            _requestChainIndex = HTTPChain.IDLE;
+            _requestChainIndex = HttpStatus.IDLE;
             GD.Print("ERROR: Request ended early");
         }
         // else if (_requestChainIndex == 2)
@@ -91,6 +94,12 @@ $@"{{
         GD.Print("sent phases");
     }
 
+    private void GetLevels()
+    {
+        _requestChainIndex = HttpStatus.GET_LEVELS;
+        _httpRequestNode.Request($"http://{_connectionIP}:3000/get_levels", method: Godot.HttpClient.Method.Get);
+    }
+
     // private void SendMusicJson(string levelID)
     // {
     //     FileAccess musicFile = FileAccess.Open($"user://levels/{levelID}/music.mp3", FileAccess.ModeFlags.Read);
@@ -108,30 +117,39 @@ $@"{{
     private void OnResponse(long result, long responseCode, string[] headers, byte[] body)
     {
         string message = System.Text.Encoding.UTF8.GetString(body);
+
+        if (_requestChainIndex == HttpStatus.GET_LEVELS)
+        {
+            GD.Print("getting");
+            GD.Print(message);
+            _requestChainIndex = HttpStatus.IDLE;
+            return;
+        }
+
         switch (_requestChainIndex)
         {
-            case HTTPChain.INDEX:
+            case HttpStatus.POST_INDEX:
                 if (responseCode == 200)
                 {
-                    _requestChainIndex = HTTPChain.PHASES;
+                    _requestChainIndex = HttpStatus.POST_PHASES;
                 }
                 else
                 {
                     // GD.Print(responseCode);
                     GD.Print(message);
-                    _requestChainIndex = HTTPChain.ERROR;
+                    _requestChainIndex = HttpStatus.POST_ERROR;
                 }
                 break;
-            case HTTPChain.PHASES:
+            case HttpStatus.POST_PHASES:
                 //END
                 if (responseCode == 200)
                 {
-                    _requestChainIndex = HTTPChain.IDLE;
+                    _requestChainIndex = HttpStatus.IDLE;
                 }
                 if (responseCode == 500)
                 {
                     GD.PrintErr(message);
-                    _requestChainIndex = HTTPChain.ERROR;
+                    _requestChainIndex = HttpStatus.POST_ERROR;
                 }
                 break;
             default:
